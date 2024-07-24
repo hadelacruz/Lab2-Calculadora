1a38e5d36d0a569d177e1c960bf90c63112a52e1
jruiz002 <josegrg04@gmail.com>
2024-07-24 17:53:09 -0600

    Se agregaron los métodos con su firma respectiva

--- Lab_2/src/main/kotlin/Main.kt
+++ Lab_2/src/main/kotlin/Main.kt
@@ -1,5 +1,25 @@
 package org.example
 
+import java.util.Stack
+import kotlin.math.*
+class CalculadoraCientifica{
+    //Método para evaluar la Expresión Infix
+    fun evaluateInfix(expression: String): Double{
+        return 0.0
+    }
+
+    // Método para convertir de infix a postFix
+    fun infixToPostfix(expression: String): List<String>{
+        return mutableListOf<String>()
+    }
+
+    // Método para evaluar la operación Postfix
+    fun evaluatePostfix(postfix: List<String>): Double{
+        return 0.0
+    }
+
+}
+
 fun main() {
     println("Hello World!")
 }
