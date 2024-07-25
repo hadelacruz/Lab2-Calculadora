0590b7ddc7f5308cebb46b5287c8ac0edf4b256a
GerardoFernandez7 <101151869+GerardoFernandez7@users.noreply.github.com>
2024-07-25 17:17:40 -0600

    Se agrego otra prueba unitaria, en la que se prueba la raiz cuadrada y la division

--- Lab_2/src/test/kotlin/CalculadoraTest.kt
+++ Lab_2/src/test/kotlin/CalculadoraTest.kt
@@ -11,4 +11,13 @@ class CalculadoraTest {
         val result = calculadora.evaluatePostfix(calculadora.stringToList(postfixExpression))
         assertEquals(7.0, result)
     }
+
+    @Test
+    fun testEvaluateComplexExpression1() {
+        val calculadora = Calculadora()
+        val infixExpression = "16^(1/2)+10"
+        val postfixExpression = calculadora.infixToPostfix(infixExpression)
+        val result = calculadora.evaluatePostfix(calculadora.stringToList(postfixExpression))
+        assertEquals(14.0, result)
+    }
 }
